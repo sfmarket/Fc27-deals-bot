ab0e13fad5073eb74cf01054ad6d681d2324a9d8
sfmarket <whileylucy@icloud.com>
2026-09-21 22:18:16 +0100

    Change test_players from list to tuple

--- bot.py
+++ bot.py
@@ -100,7 +100,7 @@ async def deal(
 )
 async def scan(interaction: discord.Interaction):
 
-    test_players = [
+test_players = (
         {
             "name": "Example Player 1",
             "current_price": 85000,
